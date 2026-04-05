749973f5b8748d7e22c6f48786f8c0db5b4f0c7d
Roypro1411 <ndonglaroyl369@gmail.com>
2026-04-05 15:38:32 +0100

    Add files via upload

---
+++ orbituary right.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number = 14;
+    if(number%2 == 0){
+        cout<<nember << " even "<< endl;
+    }
+        else{
+            cout<< number<<" odd "<< endl;
+        }
+    return 0;
+}
